dfb68542fee01b33ce79729f60fe39b5b1f0eae1
Banee Ishaque K <Baneeishaque@gmail.com>
2017-12-24 01:40:24 +0530

    fix

--- aria2c_service/aria2c_remote.cs
+++ aria2c_service/aria2c_remote.cs
@@ -25,36 +25,41 @@ namespace aria2c_service
             startInfo.UseShellExecute = false;
             startInfo.FileName = "aria2c.exe";
 
-            //string arguments = "--enable-rpc --rpc-listen-all";
-
             string arguments = "--enable-rpc --rpc-listen-all --log=aria2c_rpc.log";
 
-            //if (Directory.Exists(new KnownFolder(KnownFolderType.Downloads).DefaultPath))
+            //write_event_logs_for_application("aria2c_rpc", "Path : "+new KnownFolder(KnownFolderType.Downloads).Path, EventLogEntryType.Information);
+            //write_event_logs_for_application("aria2c_rpc", "Default Path : " + new KnownFolder(KnownFolderType.Downloads).DefaultPath, EventLogEntryType.Information);
+
+            //if (Directory.Exists(new KnownFolder(KnownFolderType.Downloads).Path))
             //{
-            //    arguments = arguments + " --dir = " + new KnownFolder(KnownFolderType.Downloads).DefaultPath;
+            //    arguments = arguments + " --dir = " + new KnownFolder(KnownFolderType.Downloads).Path;
             //}
             //else
             //{
             //    try
             //    {
             //        // Try to create the directory.
-            //        Directory.CreateDirectory(new KnownFolder(KnownFolderType.Downloads).DefaultPath);
-            //        arguments = arguments + " --dir=" + new KnownFolder(KnownFolderType.Downloads).DefaultPath;
+            //        Directory.CreateDirectory(new KnownFolder(KnownFolderType.Downloads).Path);
+            //        arguments = arguments + " --dir=" + new KnownFolder(KnownFolderType.Downloads).Path;
             //    }
             //    catch (Exception exception)
             //    {
-            //        string sSource = "aria2c_rpc";
-
-            //        if (!EventLog.SourceExists(sSource))
-            //            EventLog.CreateEventSource(sSource, "Application");
-
-            //        EventLog.WriteEntry(sSource, "Default downloads directory can't be created. Exception is " + exception.ToString(), EventLogEntryType.Error);
+            //        write_event_logs_for_application("aria2c_rpc", "Default downloads directory can't be created. Exception is " + exception.ToString(), EventLogEntryType.Error);
             //    }
             //}
 
+            //write_event_logs_for_application("aria2c_rpc", "Arguments " + arguments, EventLogEntryType.Information);
             startInfo.Arguments = arguments;
             Process.Start(startInfo);
 
+        }
+
+        void write_event_logs_for_application(string sSource, string event_message, EventLogEntryType event_type)
+        {
+            if (!EventLog.SourceExists(sSource))
+                EventLog.CreateEventSource(sSource, "Application");
+
+            EventLog.WriteEntry(sSource, event_message, event_type);
         }
 
         protected override void OnStop()
