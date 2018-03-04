89c31941e5f84f3d87a4d32a92b4ca0685bca5ee
Banee Ishaque K <Baneeishaque@gmail.com>
2018-03-04 08:13:51 +0530

    Update Server_Endpiont.cs

--- aria2c_service/Server_Endpiont.cs
+++ aria2c_service/Server_Endpiont.cs
@@ -4,7 +4,7 @@ namespace aria2c_service
 {
     class Server_Endpiont
     {
-        public static String Server_IP_Address= "http://findandgo.conut.in";
+        public static String Server_IP_Address= "http://findandgo.in";
         public static String Server_Application_Root = "aria2_remote_production";
 
     }
