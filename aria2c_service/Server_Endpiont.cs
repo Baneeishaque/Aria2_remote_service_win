7cba01d7dd45b3e7265a5690a3dffb8d1799d0c0
Banee Ishaque K <Baneeishaque@gmail.com>
2018-04-27 16:33:29 +0530

    Update Server_Endpiont.cs

--- aria2c_service/Server_Endpiont.cs
+++ aria2c_service/Server_Endpiont.cs
@@ -4,8 +4,8 @@ namespace aria2c_service
 {
     class Server_Endpiont
     {
-        public static String Server_IP_Address= "http://findandgo.in";
-        public static String Server_Application_Root = "aria2_remote_production";
+        public static String Server_IP_Address= "http://vfmob.com.md-in-64.webhostbox.net/wp-production";
+        public static String Server_Application_Root = "aria2_remote_server";
 
     }
 }
